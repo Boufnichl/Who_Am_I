2f231adf9678e2680e8a79748a1ba96977a3346e
Boufnichel314 <Boufnichel314@gmail.com>
2023-07-12 15:29:01 +0100

    the update

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,5 @@
 export const socialsData = {
     github: 'https://github.com/Boufnichel314',
-    facebook: '',
     linkedIn: 'https://www.linkedin.com/in/boufnichel-yassine/',
-    instagram: '',
-    codepen: '',
-    twitter: '',
-    reddit: '',
-    blogger: '',
-    dev: 'https://dev.to/boufnichel',
-    stackOverflow: '',
-    gitlab: '',
-    youtube: ''
+    dev: 'https://dev.to/boufnichel'
 }
